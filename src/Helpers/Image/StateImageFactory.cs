f0383c21f1b447da8c5ebbe9646e3c771393bd98
mr_nil <stephen.moretti@gmail.com>
2026-05-17 12:49:55 +0100

    fix: improve icon rendering quality using System.Drawing.Graphics

--- src/Helpers/Image/StateImageFactory.cs
+++ src/Helpers/Image/StateImageFactory.cs
@@ -2,14 +2,34 @@ namespace Loupedeck.OBSStudioForLogiPlugin
 {
     using System;
     using System.Collections.Concurrent;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
 
     internal class StateImageFactory : IActionImageFactory<StateImageData>
     {
-        private readonly ConcurrentDictionary<String, BitmapImage> iconCache;
+        private readonly Object locker;
+        private readonly ConcurrentDictionary<String, Bitmap> iconCache;
+        private readonly Bitmap imageWidth50;
+        private readonly Bitmap imageWidth80;
+        private readonly Graphics graphicsWidth50;
+        private readonly Graphics graphicsWidth80;
 
         public StateImageFactory()
         {
-            this.iconCache = new ConcurrentDictionary<String, BitmapImage>();
+            this.locker = new Object();
+            this.iconCache = new ConcurrentDictionary<String, Bitmap>();
+            this.imageWidth50 = new Bitmap(50, 50);
+            this.graphicsWidth50 = Graphics.FromImage(this.imageWidth50);
+            this.imageWidth80 = new Bitmap(80, 80);
+            this.graphicsWidth80 = Graphics.FromImage(this.imageWidth80);
+            
+            this.graphicsWidth50.SmoothingMode = SmoothingMode.AntiAlias;
+            this.graphicsWidth50.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            this.graphicsWidth50.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            
+            this.graphicsWidth80.SmoothingMode = SmoothingMode.AntiAlias;
+            this.graphicsWidth80.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            this.graphicsWidth80.PixelOffsetMode = PixelOffsetMode.HighQuality;
         }
 
         public IActionImageFactory<StateImageData> Create()
@@ -17,22 +37,45 @@ namespace Loupedeck.OBSStudioForLogiPlugin
             return new StateImageFactory();
         }
 
-        public BitmapImage DrawBitmapImage(StateImageData data, PluginImageSize imageSize)
+        private Bitmap GetIcon(String iconPath)
         {
-            String iconPath = data.IsActive ? data.ActiveIconPath : data.InactiveIconPath;
-            String cacheKey = $"{iconPath}_{imageSize}";
-
-            return this.iconCache.GetOrAdd(cacheKey, (key) =>
+            return this.iconCache.GetOrAdd(iconPath, (key) =>
             {
                 BitmapImage image = EmbeddedResources.ReadImage(iconPath);
                 if (image == null)
                 {
                     PluginLog.Warning($"Failed to load icon: '{iconPath}'");
-                    BitmapHelper.GetImageSize(imageSize, out Int32 width, out Int32 height);
-                    return BitmapHelper.ToBitmapImage(BitmapHelper.CreateBlackBitmap(width, height));
+                    return BitmapHelper.CreateBlackBitmap(50, 50);
                 }
-                return image;
+                
+                return BitmapHelper.ToBitmap(image);
             });
+        }
+
+        public BitmapImage DrawBitmapImage(StateImageData data, PluginImageSize imageSize)
+        {
+            lock (this.locker)
+            {
+                String iconPath = data.IsActive ? data.ActiveIconPath : data.InactiveIconPath;
+                Bitmap icon = this.GetIcon(iconPath);
+                
+                if (imageSize == PluginImageSize.Width60)
+                {
+                    this.graphicsWidth50.Clear(Color.Black);
+                    Int32 x = (this.imageWidth50.Width - icon.Width) / 2;
+                    Int32 y = (this.imageWidth50.Height - icon.Height) / 2;
+                    this.graphicsWidth50.DrawImage(icon, x, y, icon.Width, icon.Height);
+                    return BitmapHelper.ToBitmapImage(this.imageWidth50);
+                }
+                else
+                {
+                    this.graphicsWidth80.Clear(Color.Black);
+                    Int32 x = (this.imageWidth80.Width - icon.Width) / 2;
+                    Int32 y = (this.imageWidth80.Height - icon.Height) / 2;
+                    this.graphicsWidth80.DrawImage(icon, x, y, icon.Width, icon.Height);
+                    return BitmapHelper.ToBitmapImage(this.imageWidth80);
+                }
+            }
         }
     }
 }
