1354db86675ad0075897a48d3ca450d3a61bb86c
mr_nil <stephen.moretti@gmail.com>
2026-05-17 14:11:23 +0100

    fix: Change EmbeddedResources to PluginResources in ButtonTextRenderer

--- src/Helpers/ButtonTextRenderer.cs
+++ src/Helpers/ButtonTextRenderer.cs
@@ -22,7 +22,7 @@ namespace Loupedeck.OBSStudioForLogiPlugin
         {
             BitmapColor fgColor = textColor ?? BitmapColor.White;
             Int32 fontSize = GetLargeFontSize(imageSize);
-            BitmapImage iconImage = EmbeddedResources.ReadImage(iconResourceName);
+            BitmapImage iconImage = PluginResources.ReadImage(iconResourceName);
             
             using (var builder = new BitmapBuilder(imageSize))
             {
